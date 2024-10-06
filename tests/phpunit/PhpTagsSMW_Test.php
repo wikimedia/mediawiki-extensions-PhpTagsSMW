b3b863643f26792b2897bec3d5f8e69053eee29d
Pppery <mapreader@olum.org>
2024-10-06 22:04:06 +0000

    Try to unbreak CI
    Depends-On: I2eb88c4da31a86a86e948120aa7eaf47ca460d0d
    Depends-On: I36d29173ca7ec65a915158d053b8ce98ce8f38e1
    Change-Id: Icef25c2996083966bc29214a97289323af86cac2

--- tests/phpunit/PhpTagsSMW_Test.php
+++ tests/phpunit/PhpTagsSMW_Test.php
@@ -7,10 +7,16 @@ namespace PhpTags;
 class PhpTagsSMW_Test extends \PHPUnit\Framework\TestCase {
 
 	public function testRun_constant_1() {
+		if ( Renderer::$needInitRuntime ) {
+			\MediaWiki\MediaWikiServices::getInstance()->getHookContainer()->run( 'PhpTagsRuntimeFirstInit' );
+			Hooks::loadData();
+			Runtime::$loopsLimit = 1000;
+			Renderer::$needInitRuntime = false;
+		}
 		$this->assertEquals(
-				Runtime::runSource( 'echo PHPTAGS_SMW_VERSION;' ),
-				[ \ExtensionRegistry::getInstance()->getAllThings()['PhpTags SMW']['version'] ]
-			);
+			Runtime::runSource( 'echo PHPTAGS_SMW_VERSION;' ),
+			[ \ExtensionRegistry::getInstance()->getAllThings()['PhpTags SMW']['version'] ]
+		);
 	}
 
 }
